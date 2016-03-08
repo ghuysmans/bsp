dbd447edb51e9d14c9e2d3e6753d23f721ff7eb0
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-03-08 23:05:07 +0100

    Added manual tests

--- core/TestUI.java
+++ core/TestUI.java
@@ -2,6 +2,18 @@ package core;
 
 class TestUI {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		//TODO convert this to a unit test (and add cases!)
+		BST a = new BST();
+		BST b = new BST(new Segment(new Point(0,0), new Point(1,1)));
+		BST c = new BST();
+		a.items.add(new Segment(new Point(0,-1), new Point(1,0)));
+		c.items.add(new Segment(new Point(0,1), new Point(1,2)));
+		b.negative = a;
+		b.positive = c;
+		System.out.print(b);
+		//Segment s = new Segment(new Point(0,1), new Point(1,2));
+		Segment s = new Segment(new Point(0,2), new Point(2,0));
+		System.out.println("looking for "+s);
+		System.out.println(b.contains(s));
 	}
 }
