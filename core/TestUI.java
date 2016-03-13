7c289cbfccb509fb2c5dfc157693a1121b8f4b64
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-03-13 17:05:48 +0100

    Created a UI template

--- core/TestUI.java
+++ core/TestUI.java
@@ -1,19 +1,41 @@
 package core;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-class TestUI {
+class TestUI extends JFrame implements ActionListener {
+	protected final JMenuBar menuBar = new JMenuBar();
+	protected final JMenu menuHelp = new JMenu("Help");
+	protected final JMenuItem menuManual = new JMenuItem("Manual");
+	protected final JMenuItem menuAbout = new JMenuItem("About...");
+
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(this,
+			"By Guillaume Huysmans, 2016.\nBased on"+
+			"Computational Geometry (Mark de Berg et al.), chapter 12.",
+			"About...", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	protected void initMenus() {
+		menuBar.add(menuHelp);
+		menuHelp.add(menuAbout);
+		menuAbout.addActionListener(this);
+		setJMenuBar(menuBar);
+	}
+
+	public TestUI() {
+		setTitle("BSP Viewer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 300);
+		setVisible(true);
+		initMenus();
+	}
+
 	public static void main(String[] args) {
-		//TODO convert this to a unit test (and add cases!)
-		BST a = new BST();
-		BST b = new BST(new Segment(new Point(0,0), new Point(1,1)));
-		BST c = new BST();
-		a.items.add(new Segment(new Point(0,-1), new Point(1,0)));
-		c.items.add(new Segment(new Point(0,1), new Point(1,2)));
-		b.negative = a;
-		b.positive = c;
-		System.out.print(b);
-		//Segment s = new Segment(new Point(0,1), new Point(1,2));
-		Segment s = new Segment(new Point(0,2), new Point(2,0));
-		System.out.println("looking for "+s);
-		System.out.println(b.contains(s));
+		new TestUI();
 	}
 }
