72e84553137d42fa04199bf9fd29b672e4af43ab
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-04-02 21:31:59 +0200

    Precision

--- core/Point.java
+++ core/Point.java
@@ -1,7 +1,7 @@
 package core;
 
 public class Point {
-	public static final float EPSILON = 0.00001F;
+	public static final float EPSILON = 5E-4F;
 	public static final Point ORIGIN = new Point(0, 0);
 
 	public final float x;
