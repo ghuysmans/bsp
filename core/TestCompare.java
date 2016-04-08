91121e6cea282434d79ed172e56ab323a32687a7
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-04-08 02:30:23 +0200

    Implemented a basic TUI using StreamTokenizer

--- core/TestCompare.java
+++ core/TestCompare.java
@@ -1,7 +1,12 @@
 package core;
-import java.util.Scanner;
+import java.io.StreamTokenizer;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
+import java.lang.reflect.Method;
 
 /**
  * Main class for the text-based heuristic comparison tool
@@ -9,31 +14,133 @@ import java.lang.management.ThreadMXBean;
 class TestCompare {
 	protected static final double NANO_TO_SEC = 1E9;
 	protected final ThreadMXBean timer;
+	protected final StreamTokenizer st;
+	protected Scene scene;
 
+	/**
+	 * Report a result
+	 */
 	protected void report(String descr, long ref_cpu, long ref_wallclock) {
 		double cpu = (timer.getCurrentThreadCpuTime()-ref_cpu) / NANO_TO_SEC;
 		double wallclock = (System.nanoTime()-ref_wallclock) / NANO_TO_SEC;
 		System.out.printf("%-20s%.2f\t%.2f\n", descr, cpu, wallclock);
 	}
 
-	public TestCompare(String scene) {
-		Scanner sc = new Scanner(System.in);
-		timer = ManagementFactory.getThreadMXBean();
-		try {
-			while (true) {
-				System.out.print(">>> ");
-				sc.next();
-				long t = timer.getCurrentThreadCpuTime();
-				long w = System.nanoTime();
-				Thread.currentThread().sleep(2000);
-				report("sleep 2K", t, w);
-			}
-		}
-		catch (Exception e) {
+	/**
+	 * Discard the current line
+	 */
+	protected void discardLine() throws IOException {
+		while (st.nextToken()!=StreamTokenizer.TT_EOF &&
+				st.ttype!=StreamTokenizer.TT_EOL);
+	}
+
+	/**
+	 * Try to match the next token with the given type.
+	 * If it fails, discard the whole line.
+	 * @return true when they match
+	 */
+	protected boolean match(int ttype) throws IOException {
+		if (st.nextToken() == ttype)
+			return true;
+		else {
+			System.out.println("syntax error");
+			//System.out.println("expected type "+ttype+", got "+st);
+			discardLine();
+			return false;
 		}
 	}
 
+	/**
+	 * Display a help message
+	 */
+	protected void help(boolean err) {
+		System.out.println("available commands:");
+		for (Method m: getClass().getDeclaredMethods()) {
+			String name = m.getName();
+			if (name.startsWith("do_"))
+				System.out.println(name.substring(3));
+		}
+	}
+
+	/**
+	 * Load a scene or display an error message
+	 */
+	public void loadScene(String filename) {
+		try {
+			scene = new Scene(filename);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	protected void do_help() throws IOException {
+		help(false);
+		match(StreamTokenizer.TT_EOL);
+	}
+
+	protected void do_exit() throws IOException {
+		System.exit(0);
+	}
+
+	protected void do_quit() throws IOException {
+		System.exit(0);
+	}
+
+	protected void do_load() throws IOException {
+		if (!match('"'))
+			return;
+		loadScene(st.sval);
+		match(StreamTokenizer.TT_EOL);
+	}
+
+	/**
+	 * Command interpreter loop
+	 */
+	public void loop() {
+		System.out.print(">>> ");
+		try {
+			while (st.nextToken() != StreamTokenizer.TT_EOF) {
+				if (st.ttype != StreamTokenizer.TT_WORD) {
+					System.out.println("expected a command!");
+					discardLine();
+					continue;
+				}
+				String name = "do_"+st.sval.toLowerCase();
+				try {
+					//getMethod won't find protected methods
+					getClass().getDeclaredMethod(name).invoke(this);
+				}
+				catch (NoSuchMethodException e) {
+					System.out.println("unknown command: "+st.sval);
+					help(true);
+					discardLine();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+					assert(false);
+				}
+				/*
+				long t = timer.getCurrentThreadCpuTime();
+				long w = System.nanoTime();
+				report("sleep 2K", t, w);
+				*/
+				System.out.print(">>> ");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public TestCompare(String scene) {
+		Reader r = new BufferedReader(new InputStreamReader(System.in));
+		timer = ManagementFactory.getThreadMXBean();
+		st = new StreamTokenizer(r);
+		st.eolIsSignificant(true);
+	}
+
 	public static void main(String[] args) {
-		new TestCompare(args.length==1 ? args[0] : null);
+		new TestCompare(args.length==1 ? args[0] : null).loop();
 	}
 }
