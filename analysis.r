ba20e223cf8bbce72988d63d20510f08f615cf36
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-04-10 15:33:57 +0200

    Renamed axes

--- analysis.r
+++ analysis.r
@@ -4,10 +4,12 @@ options=list(make_option(c("-e", "--exclude"), help="exclude a heuristic"))
 parser=OptionParser("usage: %prog [options] data", options)
 args=parse_args(parser, positional_arguments=T)
 
-t=read.csv(args$args[1])
+filename=args$args[1]
+t=read.csv(filename)
 if (!is.null(args$options$exclude))
 	t=t[t$Heuristic!=args$options$exclude,]
 summary(t)
 
 library(ggplot2)
-ggplot(t, aes(x=S, fill=Heuristic)) + geom_histogram()
+ggplot(t, aes(x=S, fill=Heuristic)) + geom_histogram() +
+	labs(x="Total Segments", y="Count") #+ labs(title=filename)
